2841c2e0f5ffcb6c22c565c0466d1aad37270665
Elvina <Elle-elle7@yandex.ru>
2024-03-24 16:56:58 +0500

    задача employee_2.py не решена

--- Chapter_4/oop_practics/lesson4/employee_2.py
+++ Chapter_4/oop_practics/lesson4/employee_2.py
@@ -10,26 +10,47 @@ class Employee:
     def __init__(self, pay):
         self.pay = pay
 
+    def __add__(self, other):
+        if isinstance(other, Employee):
+            return self.pay + other.pay
+        # else:
+            # raise TypeError('Unsupported operand type(s) for +: {} and {}'.format(type(self), type(other)))
+        elif isinstance(other, int):
+            return self.pay + other
+        else:
+            raise TypeError('Unsupported operand type(s) for +: {} and {}'.format(type(self), type(other)))
+
 
 class Client:
 
     def __init__(self, pay):
         self.pay = pay
 
+    # def __add__(self):
+    #     return self.pay
+
 
 class Developer(Employee):
-    pass
+
+    def __init__(self, pay):
+        super().__init__(pay)
 
 
 class Manager(Employee):
-    pass
+
+    def __init__(self, pay):
+        super().__init__(pay)
+
 
 # код для проверки
-users = [Employee(50000), Client(100000), Developer(50000), Manager(50000)]
+# users = [Employee(50000), Client(100000), Developer(50000), Manager(50000)]
+users = [Employee(10000), Developer(20000), Manager(30000), Client(100000)]
 
 total_salary = 0
 for user in users:
-    total_salary = total_salary + user
+    # print(user.pay)
+    total_salary = user + total_salary
+
 
 print(total_salary)
 # Вывод: 150000
