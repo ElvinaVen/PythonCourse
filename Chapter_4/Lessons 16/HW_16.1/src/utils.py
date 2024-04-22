1710b1fca6f0cbed5f1cf39a21f1c1399aecdf33
Elvina <Elle-elle7@yandex.ru>
2024-04-22 21:36:37 +0500

    ДЗ 16.1 выполнена

---
+++ Chapter_4/Lessons 16/HW_16.1/src/utils.py
@@ -0,0 +1,12 @@
+import json
+
+
+def load_src_file():
+    """
+    Ф-ия загрузки списка из json.
+    :return: src_file - весь список категорий с описанием и товарами
+    """
+    # with open('file_fruits.json', 'r', encoding='utf-8') as file:
+    with open('file.json', 'r', encoding='utf-8') as file:
+        src_file = json.load(file)
+    return src_file
