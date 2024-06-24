14c0c41affdf1d2f67d8b3dbf374da160baff208
Elvina <Elle-elle7@yandex.ru>
2024-06-25 02:48:13 +0500

    22.1.3. * Формсеты выполнено

--- Chapter_6/Lessons20/project6_1/main/views.py
+++ Chapter_6/Lessons20/project6_1/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
+from django.forms import inlineformset_factory
 
 from main.models import Student
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from main.forms import StudentForm
+
+from main.models import Subject
+
+from main.forms import SubjectForm
 
 
 class StudentListView(ListView):
@@ -42,6 +47,24 @@ class StudentUpdateView(UpdateView):
     model = Student
     form_class = StudentForm
     success_url = reverse_lazy('main:index')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Student, Subject, form=SubjectForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = SubjectFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
 
 
 class StudentDeleteView(DeleteView):
