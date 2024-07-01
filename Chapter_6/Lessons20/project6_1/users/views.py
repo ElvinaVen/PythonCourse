2ee588b5f13fc7a37974b7fa34d8a1c9d588caee
Elvina <Elle-elle7@yandex.ru>
2024-07-02 02:02:30 +0500

    22.2 Аутентификация выполнено

---
+++ Chapter_6/Lessons20/project6_1/users/views.py
@@ -0,0 +1,27 @@
+
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from users.models import User
+
+from users.forms import UserRegisterForm
+
+from users.forms import UserProfileForm
+
+
+# Create your views here.
+class RegisterView(generic.CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileView(generic.UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self):
+        return self.request.user
