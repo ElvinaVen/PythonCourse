9cb1d6b5491e353dac2c0fbd50534e04e740321f
Elvina <Elle-elle7@yandex.ru>
2024-07-03 02:12:59 +0500

    дз аутентификация выполнено(кроме задания 3)

--- Chapter_6/HW_Chapter6/users/urls.py
+++ Chapter_6/HW_Chapter6/users/urls.py
@@ -7,11 +7,15 @@ from users.views import RegisterView, email_verification
 
 from users.views import ProfileView
 
+from users.views import reset_password
+
 app_name = UsersConfig.name
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('reset_password/', reset_password, name='reset_password'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- Chapter_6/HW_Chapter6/users/views.py
+++ Chapter_6/HW_Chapter6/users/views.py
@@ -1,7 +1,11 @@
 import secrets
+import string
+import random
 
+# from django.contrib.auth.forms import PasswordResetForm
+# from django.contrib.auth.views import PasswordResetView
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
@@ -50,3 +54,30 @@ class ProfileView(UpdateView):
 
     def get_object(self):
         return self.request.user
+
+
+def reset_password(request):
+    context = {
+        'success_message': 'Пароль успешно сброшен. Новый пароль был отправлен на адрес Вашей электронной почты.',
+    }
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = get_object_or_404(User, email=email)
+        characters = string.ascii_letters + string.digits
+        characters_list = list(characters)
+        random.shuffle(characters_list)
+        password = ''.join(characters_list[:10])
+        user.set_password(password)
+        user.save()
+
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Вот новый пароль: {password}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+
+        return render(request, 'users/reset_password.html', context)
+
+    return render(request, 'users/reset_password.html')
