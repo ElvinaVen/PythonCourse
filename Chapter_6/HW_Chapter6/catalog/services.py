db60737b54dbc7d02e07bb3c783e1bd924d5886a
elvina <Elle-elle7@yandex.ru>
2024-07-19 00:54:32 +0500

    23.2 Кеширование и работа с переменными окружения. ДЗ выполнено!

--- Chapter_6/HW_Chapter6/catalog/services.py
+++ Chapter_6/HW_Chapter6/catalog/services.py
@@ -5,12 +5,13 @@ from catalog.models import Product
 
 
 def get_cached_products():
+    ''' Получает данные по подуктам из кеша, если кеш пуст, то получает из БД '''
     if settings.CACHE_ENABLED:
-        key = f'subject_list_{student_pk}'
+        key = f'subject_list'
         subject_list = cache.get(key)
         if subject_list is None:
-            subject_list = Product.objects.filter(student__pk=student_pk)
+            subject_list = Product.objects.all()
             cache.set(key, subject_list)
     else:
-        subject_list = Product.objects.filter(student__pk=student_pk)
+        subject_list = Product.objects.all()
     return subject_list
